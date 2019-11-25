930798236d99e0f87d1000ac4c2d41ce14a35ac5
valter <vammartins@inf.ufpel.edu.br>
2019-11-25 19:37:35 -0300

    adicionando codigo para resgatar letras para o dataset, a partir do url

---
+++ pega_letra.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def main():
+    url = 'https://www.letras.mus.br/billie-eilish/everything-i-wanted/traducao.html'
+    html = urlopen(url)
+    soup = BeautifulSoup(html.read())
+    list_en = []
+    list_pt = []
+
+    todo_en = soup.find("div", {"class": "cnt-trad_l"})
+    todo_pt = soup.find("div", {"class": "cnt-trad_r"})
+
+    #pega a letra em ingles
+    tit_en = str(todo_en.findNext("h3"))
+    tit_en = tit_en.replace("<h3>", "")
+    tit_en = tit_en.replace("</h3>", "")
+    list_en.append(tit_en)
+
+    letra_en = todo_en.findAll('span')
+    for x in letra_en:
+        y = str(x)
+        y = y.replace("<span>","")
+        y = y.replace("</span>",".")
+        list_en.append(y)
+
+    print(list_en)
+
+    #pega a letra em portugues
+    tit_pt = str(todo_pt.findNext("h3"))
+    tit_pt = tit_pt.replace("<h3>", "")
+    tit_pt = tit_pt.replace("</h3>", "")
+    list_pt.append(tit_pt)
+
+    letra_pt = todo_pt.findAll('span')
+    for x in letra_pt:
+        y = str(x)
+        y = y.replace("<span>","")
+        y = y.replace("</span>",".")
+        list_pt.append(y)
+
+    print(list_pt)
+
+    #converte para zip, depois lista, e depois em dataframe
+    dataset = zip(list_en, list_pt)
+    dataset = list(dataset)
+    data_set = pd.DataFrame(dataset, columns=['en_US', 'pt_BR'])
+    data_set
+
+
+
+if __name__ == "__main__":
+    main()
