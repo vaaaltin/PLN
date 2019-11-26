47186c7fe81ad1ebee84a6c3b5c95ad729ac2179
valter <vammartins@inf.ufpel.edu.br>
2019-11-26 12:48:19 -0300

    algoritmo para pegar letras do site letras.mus.br a partir do link

--- Trabalho_2/pega_letra.py
+++ Trabalho_2/pega_letra.py
@@ -1,18 +1,31 @@
 import pandas as pd
 import numpy as np
+import sys
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 def main():
-    arquivo = open(str(argv[1])+'link.txt', 'r')
-    letras = arquivo.read()
+    lista_en = []
+    lista_pt = []
 
-    for x in letras:
-        pega_letra(x)
+    with open(str(sys.argv[1]+'links.txt')) as file:
+        for line in file:
+            print(line)
+            temp_en, temp_pt = pega_letra(line)
+            lista_en += temp_en
+            lista_pt += temp_pt
+    
+    #converte para zip, depois para lista, e depois em dataframe
+    
+    dicionario = {'en_us':lista_en, 'pt_BR':lista_pt}
+    data_set = pd.DataFrame(dicionario)
+    #print(data_set)
+    data_set.to_csv(str(sys.argv[1]+'dataset.txt'), sep='\t', mode='w', index=False)
 
 def pega_letra(url):
+    #print(url)
     html = urlopen(url)
-    soup = BeautifulSoup(html.read())
+    soup = BeautifulSoup(html.read(), 'html.parser')
     list_en = []
     list_pt = []
 
@@ -47,17 +60,7 @@ def pega_letra(url):
         y = y.replace("</span>",".")
         list_pt.append(y)
 
-    #print(list_pt)
-
-    #converte para zip, depois para lista, e depois em dataframe
-    dataset = zip(list_en, list_pt)
-    dataset = list(dataset)
-    data_set = pd.DataFrame(dataset, columns=['en_US', 'pt_BR'])
-    #data_set
-    arq = open()
-
-
-
+    return list_en, list_pt
 
 if __name__ == "__main__":
     main()
